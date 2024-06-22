49ff9547e3926d2c39374e394430060f9235c82d
yali <tangozyusufali@gmail.com>
2024-06-23 01:21:46 +0300

    feat: init app

---
+++ README.md
@@ -0,0 +1,1 @@
+# nuxt-shadcn-boilerplate
---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <NuxtLoadingIndicator />
+    <NuxtRouteAnnouncer />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </main>
+</template>
