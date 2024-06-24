576e859203b23ba2b0fc781398d44d5026c04f78
yali <tangozyusufali@gmail.com>
2024-06-25 02:07:38 +0300

    style: update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -25,29 +25,42 @@ const socials = [
 </script>
 
 <template>
-  <footer class="bg-zinc-100 dark:bg-black border-t-[1px] w-full">
+  <footer class="bg-zinc-100 dark:bg-black border-t-[1px] w-full text-sm">
     <section class="container p-10 grid md:grid-cols-2 gap-10">
-      <section class="flex flex-col gap-2 place-items-start">
+      <section class="grid place-items-start gap-3">
         <Logo />
-        <p class="text-sm">
+        <p class="text-zinc-500 dark:text-zinc-400">
           {{ $t("footer.description") }}
         </p>
         <section class="flex flex-wrap gap-2">
-          <NuxtLink
+          <Button
             v-for="{ icon, href } in socials"
             :key="icon"
-            target="_blank"
-            :to="href"
+            class="w-8 h-8"
+            variant="outline"
+            size="icon"
+            as-child
           >
-            <Icon :name="icon" size="17" />
-          </NuxtLink>
+            <NuxtLink target="_blank" :to="href">
+              <Icon :name="icon" size="17" />
+            </NuxtLink>
+          </Button>
         </section>
       </section>
       <section class="flex flex-wrap gap-10 md:justify-end">
-        <ul v-for="{ title, links } in linkGroups" :key="title">
-          <h1 class="font-semibold mb-2">{{ $t(title) }}</h1>
-          <li v-for="{ href, name } in links" :key="name" class="text-sm">
-            <NuxtLink :to="href">{{ $t(name) }}</NuxtLink>
+        <ul
+          v-for="{ title, links } in linkGroups"
+          :key="title"
+          class="space-y-3"
+        >
+          <h1 class="font-semibold">{{ $t(title) }}</h1>
+          <li v-for="{ href, name } in links" :key="name">
+            <NuxtLink
+              class="text-zinc-500 dark:text-zinc-400 hover:text-black dark:hover:text-white transition-colors"
+              :to="href"
+            >
+              {{ $t(name) }}
+            </NuxtLink>
           </li>
         </ul>
       </section>
