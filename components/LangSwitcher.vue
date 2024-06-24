702e274f3ca4d3ec6f57620bc0df64b149df4a99
yali <tangozyusufali@gmail.com>
2024-06-24 18:45:36 +0300

    feat: lang switcher

---
+++ components/LangSwitcher.vue
@@ -0,0 +1,29 @@
+<script setup>
+const switchLocalePath = useSwitchLocalePath();
+const { locale, locales } = useI18n();
+
+const availableLocales = computed(() => {
+  return locales.value.filter((i) => i.code !== locale.value);
+});
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button size="icon" variant="outline">
+        <Icon name="iconoir:language" size="18" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuItem
+        v-for="{ code, name } in availableLocales"
+        :key="code"
+        as-child
+      >
+        <NuxtLink :to="switchLocalePath(code)">
+          {{ name }}
+        </NuxtLink>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
