170255e15ecf6aa56997cb1cc7ff218445a53f92
yali <tangozyusufali@gmail.com>
2024-06-24 18:45:19 +0300

    feat: logo

---
+++ components/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLink to="/">
+    <NuxtImg
+      src="https://nuxt.com/assets/design-kit/icon-green.svg"
+      class="w-10"
+      alt="logo"
+    />
+  </NuxtLink>
+</template>
