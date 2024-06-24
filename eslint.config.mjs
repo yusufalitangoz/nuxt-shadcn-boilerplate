a33a56deefed31dca5788ba65a3c6720d2c6df3b
yali <tangozyusufali@gmail.com>
2024-06-24 18:44:51 +0300

    chore: update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,8 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt();
+export default withNuxt({
+  rules: {
+    "vue/require-default-prop": "off",
+    "vue/multi-word-component-names": "off",
+  },
+});
