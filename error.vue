00531b7c05bb3c9d57eea01a48e26851bed958c5
yali <tangozyusufali@gmail.com>
2024-06-28 04:00:53 +0300

    feat(error page): add localePath to link

--- error.vue
+++ error.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import type { NuxtError } from "#app";
-
 defineProps({
   error: Object as () => NuxtError,
 });
+const localePath = useLocalePath();
 </script>
 
 <template>
   <main class="h-screen flex justify-center items-center flex-col">
     <h1 class="text-4xl font-extrabold mb-5">{{ error.statusCode }}</h1>
     <Button as-child>
-      <NuxtLink to="/">
+      <NuxtLink :to="localePath('/')">
         {{ $t("errorPage.button") }}
       </NuxtLink>
     </Button>
